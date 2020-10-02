88468d03f418fc6f24219a233b68b451c425c390
ibrahim <ibrahim.malada@9spl.com>
2020-10-02 12:31:40 +0530

    init.

--- app/src/main/java/com/allinonedeliveryapp/ui/LoginSignup.kt
+++ app/src/main/java/com/allinonedeliveryapp/ui/LoginSignup.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.allinonedeliveryapp.R
 
 class LoginSignup : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_signup)
