ad91903151c498d6194557f3d94646df7d69854e
nkgohil <nikhilgohil123@gmail.com>
2020-10-02 19:08:30 +0530

    description screen done

---
+++ app/src/main/java/com/allinonedeliveryapp/ui/ProductClickActivity.kt
@@ -0,0 +1,12 @@
+package com.allinonedeliveryapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.allinonedeliveryapp.R
+
+class ProductClickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_click)
+    }
+}
